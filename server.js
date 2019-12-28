58df189f1011deaf291cd6e505a4e90ae626d0ef
daesy <daesyramos@hotmail.com>
2019-12-28 00:24:18 -0800

    fix app.js

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// express package calls http messages
+const express = require('express');
+// creating a server/app with express we call invoke express()
+const app = express();
+
+// send statics websites from this folder
+app.use(express.static('index.html'));
+
+// ROUTES are the addresss/locations that respond to a request
+app.get('/gingerkisses', function(request, response){
+  response.sendFile(__dirname + 'index.html');
+});
+
+const PORT = process.env.PORT  || 3000;
+
+// if I want the app to exist and run I'll tell it to listen
+app.listen(PORT, function() {
+  console.log('GOOD JOB!');
+});
